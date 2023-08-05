9f414c80807a15767787cff98badfcb177b41469
JariKooij <jarikooij@gmail.com>
2023-08-06 01:28:26 +0200

    serverless test 2

--- src/app/projects/[id]/page.tsx
+++ src/app/projects/[id]/page.tsx
@@ -1,6 +1,6 @@
 import PageWrapper from "@/components/common/PageWrapper";
 import ProjectDetailsPage from "@/components/project-details/ProjectDetailsPage";
-import projects from "@/data/projects.json";
+// import projects from "@/data/projects.json";
 import { notFound } from "next/navigation";
 import * as fs from "fs";
 import { processMarkdown } from "@/utils";
@@ -13,17 +13,72 @@ type TProps = {
     };
 };
 
-export const generateStaticParams = async () => {
-    return [];
+const projects = [
+    {
+        id: 1,
+        featured: true,
+        title: "Capgemini internships platform",
+        companyName: "Capgemini NL",
+        contributions: ["Front-end development", "UX/UI design"],
+        skills: ["Next.js", "TypeScript", "SCSS", "Figma", "Scrum"],
+        iconUrl: "/icons/projects/capgemini.svg",
+        iconHeight: 40,
+        iconWidth: 43,
+        demoUrl: "https://www.google.com",
+        discussionPath: "/markdown/discussion-1.md",
+        images: ["/images/projects/capgemini/1.jpg", "/images/projects/capgemini/2.jpg"],
+        hidden: false,
+    },
+    {
+        id: 2,
+        featured: true,
+        title: "DDAMA AR Art-Tour APP",
+        companyName: "Dutch Digital Art Museum Almere",
+        contributions: ["App development", "Front-end development", "UX/UI design"],
+        skills: ["Unity", "C#", "React", "SCSS", "Figma"],
+        iconUrl: "/icons/projects/ddama.svg",
+        iconHeight: 40,
+        iconWidth: 40,
+        discussionPath: "/markdown/discussion-1.md",
+        videoUrl: "https://www.youtube.com/watch?v=p01JA8ch2Ns",
+        hidden: false,
+    },
+    {
+        id: 3,
+        featured: false,
+        title: "Salesprof",
+        companyName: "Capgemini",
+        contributions: ["Front-end development", "Backend development"],
+        skills: ["Next.js", "TypeScript", "SCSS", "Figma"],
+        iconUrl: "/icons/projects/capgemini.svg",
+        iconHeight: 40,
+        iconWidth: 43,
+        discussionPath: "/markdown/discussion-1.md",
+        hidden: false,
+    },
+    {
+        id: 4,
+        featured: false,
+        title: "Personal portfolio",
+        companyName: "Dutch Digital Art Museum Almere",
+        contributions: ["App development", "Front-end development", "UX/UI design"],
+        skills: ["Unity", "C#", "React", "SCSS", "Figma"],
+        iconUrl: "/icons/projects/ddama.svg",
+        iconHeight: 40,
+        iconWidth: 40,
+        discussionPath: "/markdown/discussion-1.md",
+        hidden: false,
+    },
+];
 
+export const generateStaticParams = async () => {
     return projects.map((project) => ({
         id: project.id.toString(),
     }));
 };
 
 const fetchProject = (id: string) => {
-    // const project = projects.find((project) => project.id === parseInt(id));
-    let project = undefined;
+    const project = projects.find((project) => project.id === parseInt(id));
     if (!project) notFound();
 
     return project;
