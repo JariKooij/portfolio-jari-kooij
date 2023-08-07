8e41fadcd36d192c50083d27aaf6e03b3185d5dc
JariKooij <jarikooij@gmail.com>
2023-08-07 16:02:19 +0200

    a11y

--- src/components/ui/SubTitle.tsx
+++ src/components/ui/SubTitle.tsx
@@ -4,9 +4,9 @@ type TProps = {
 
 const SubTitle: React.FC<TProps> = ({ children }) => {
     return (
-        <p className="mb-16 mt-8 max-w-max whitespace-pre-wrap text-16px font-light sm:text-20px lg:text-24px">
+        <h2 className="mb-16 mt-8 max-w-max whitespace-pre-wrap text-16px font-light sm:text-20px lg:text-24px">
             {children}
-        </p>
+        </h2>
     );
 };
 
